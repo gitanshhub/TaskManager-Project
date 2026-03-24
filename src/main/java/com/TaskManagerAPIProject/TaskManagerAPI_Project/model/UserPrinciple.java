fb0fce6c95c0659787702bd949eada1e8b677e4d
Ansh <140439219+gitanshhub@users.noreply.github.com>
2026-03-24 15:43:10 +0530

    Clean up commented code in UserPrinciple.java
    Removed commented-out code for role handling in UserPrinciple.

--- src/main/java/com/TaskManagerAPIProject/TaskManagerAPI_Project/model/UserPrinciple.java
+++ src/main/java/com/TaskManagerAPIProject/TaskManagerAPI_Project/model/UserPrinciple.java
@@ -23,13 +23,6 @@ public class UserPrinciple implements UserDetails {
         return user.getRole().stream().map(role ->
                         new SimpleGrantedAuthority("ROLE_" + role.name()))
                 .collect(Collectors.toList());
-
-//        if(user.getRole() == null){
-//            return List.of();
-//        }
-//        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
-
-//        return Collections.singleton(new SimpleGrantedAuthority("USER"));
     }
 
     @Override
